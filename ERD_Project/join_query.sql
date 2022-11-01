56816d08f27fb85b153888b1ed2fc384c8c290c9
unknown <piffardb@gmail.com>
2022-10-31 20:23:04 -0700

    Minor alterations

--- ERD_Project/join_query.sql
+++ ERD_Project/join_query.sql
@@ -1,31 +1,15 @@
--- Creating table to hold all of the data
-
-SELECT promotion_id,
-	promotion_name,
-	media_type,
-	cost,
-	store_sales_in_millions,
-	store_cost_in_millions,
-	unit_sales_in_millions,
-	product_id,
-	store_id,
-	customer_id,
-	location_id 
-INTO full_data
-FROM promo_data;
-
 -- Joining all of the tables to full_data (left join)
-SELECT full_data.promotion_id,
-	full_data.promotion_name,
-	full_data.media_type,
-	full_data.cost,
-	full_data.store_sales_in_millions,
-	full_data.store_cost_in_millions,
-	full_data.unit_sales_in_millions,
-	full_data.product_id,
-	full_data.store_id,
-	full_data.customer_id,
-	full_data.location_id,
+SELECT promo_data.promotion_id,
+	promo_data.promotion_name,
+	promo_data.media_type,
+	promo_data.cost,
+	promo_data.store_sales_in_millions,
+	promo_data.store_cost_in_millions,
+	promo_data.unit_sales_in_millions,
+	promo_data.product_id,
+	promo_data.store_id,
+	promo_data.customer_id,
+	promo_data.location_id,
 	location_data.store_city, 
 	location_data.store_state,
 	customer_data.marital_status,
@@ -58,10 +42,11 @@ SELECT full_data.promotion_id,
 	product_data.recyclable_package,
 	product_data.low_fat,
 	product_data.units_per_case
-FROM full_data
-LEFT JOIN location_data ON full_data.location_id = location_data.location_id
-LEFT JOIN customer_data ON full_data.customer_id = customer_data.customer_id 
-LEFT JOIN store_data ON full_data.store_id = store_data.store_id
-LEFT JOIN product_data ON full_data.product_id = product_data.product_id;
+INTO full_dataset
+FROM promo_data
+LEFT JOIN location_data ON promo_data.location_id = location_data.location_id
+LEFT JOIN customer_data ON promo_data.customer_id = customer_data.customer_id 
+LEFT JOIN store_data ON promo_data.store_id = store_data.store_id
+LEFT JOIN product_data ON promo_data.product_id = product_data.product_id;
 
-SELECT * FROM full_data;
+SELECT * FROM full_dataset;
